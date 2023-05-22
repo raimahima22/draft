17c2da8ad02d73c9594bb9a206cb236cbec85882
raimahima22 <mahimabayingrai22@gmail.com>
2023-05-22 13:24:59 +0545

    portfolio website

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var typingEffect = new Typed(".multiText",{
+    strings : ["learner","student","designer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500,
+});
+function redirectToWebsite(){
+    window.location.href="https://wordpress.com/posts/mahimarai7.wordpress.com";
+}
+
